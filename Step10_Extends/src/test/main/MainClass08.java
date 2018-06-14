288a380b60c71ecf8ad49aa602fb52d905eb1fde
oli999 <oli999@naver.com>
2018-06-14 15:19:28 +0900

    Step10_Extends 클래스 추가됨

--- Step10_Extends/src/test/main/MainClass08.java
+++ Step10_Extends/src/test/main/MainClass08.java
@@ -1,5 +1,6 @@
 package test.main;
 
+import test.mypac.MyNavi;
 import test.mypac.Navi;
 
 public class MainClass08 {
@@ -9,7 +10,8 @@ public class MainClass08 {
 		 *  2. 그 클래스로 생성한 객체의 참조값을 얻어내서
 		 *  3. 아래의 useNavi 메소드를 호출해 보세요.
 		 */
-		
+		MyNavi n=new MyNavi();
+		MainClass08.useNavi(n);
 	}
 	
 	public static void useNavi(Navi navi) {
