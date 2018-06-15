9f4c8277770e54f2b61b3b30c4ee0efb82457b20
oli999 <oli999@naver.com>
2018-06-15 12:22:45 +0900

    Step12_InnerClass 추가됨

---
+++ Step12_InnerClass/src/test/main/MainClass03.java
@@ -0,0 +1,35 @@
+package test.main;
+
+public class MainClass03 {
+	// Inner Class
+	class Gura{
+		
+	}
+	
+	public static void main(String[] args) {
+		/*
+		 * 메소드 안에 클래스를 정의할수도 있다.
+		 * 
+		 * Local Inner Class
+		 */
+		class Car{
+			//메소드
+			public void drive() {
+				System.out.println("달려요");
+			}
+		}
+		//객체 생성후 
+		Car car=new Car();
+		//메소드 호출 가능 
+		car.drive();
+		
+	}
+}
+
+
+
+
+
+
+
+
