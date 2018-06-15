da937eb925d87b5be3fb7356a8f310deadeca931
oli999 <oli999@naver.com>
2018-06-15 12:23:45 +0900

    Step13_AbstractClass 추가됨

---
+++ Step13_AbstractClass/src/test/main/MainClass03.java
@@ -0,0 +1,24 @@
+package test.main;
+
+import test.mypac.Weapon;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		// 수열씨를 공격하고 싶다면?
+		Weapon w1=new Weapon() {
+			@Override
+			public void attack() {
+				System.out.println("수열씨를 공격해요");
+			}
+		};
+		useWeapon(w1);
+	}
+	
+	public static void useWeapon(Weapon w) {
+		w.prepare();
+		w.attack();
+	}
+}
+
+
+
