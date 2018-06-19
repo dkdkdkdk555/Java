e72ae67a16fb67191c23ff99882629a7b1c786f1
oli999 <oli999@naver.com>
2018-06-19 11:31:56 +0900

    Step17_UtilClass 수정됨

--- Step17_UtilClass/src/test/main/MainClass12.java
+++ Step17_UtilClass/src/test/main/MainClass12.java
@@ -1,6 +1,8 @@
 package test.main;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /*
  *  [ HashSet ]
@@ -14,7 +16,7 @@ import java.util.HashSet;
 public class MainClass12 {
 	public static void main(String[] args) {
 		//정수를 저장할수 있는 HashSet 객체 
-		HashSet<Integer> set1=new HashSet<>();
+		Set<Integer> set1=new HashSet<>();
 		set1.add(10);
 		set1.add(20);
 		set1.add(30);
@@ -22,8 +24,37 @@ public class MainClass12 {
 		set1.add(20);
 		set1.add(40);
 		set1.add(50);
+		/*
+		 * HashSet 객체에 있는 모든 숫자를 콘솔에 출력해 보세요.
+		 * 
+		 * 검색!
+		 */
+		
+		// Iterator(반복자) 객체 
+		Iterator<Integer> iter=set1.iterator();
+		
+		while(iter.hasNext()) {// 다음 데이터가 있는지 확인해서 
+			//있다면 다음 데이터 불러와서 출력하기 
+			System.out.println(iter.next());
+		}
+		
+		System.out.println("-- 확장 for 문도 이용할수 있다. ");
+		for(int tmp:set1) {
+			System.out.println(tmp);
+		}
+		
+		
 	}
 }
+
+
+
+
+
+
+
+
+
 
 
 
